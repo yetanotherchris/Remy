556e785ed365e20672bf39eb809f157f8586a27c
Chris Small <chris.small@totaljobsgroup.com>
2016-11-18 12:16:29 +0000

    powershell runner

--- src/Ainsley.Core/Tasks/PowershellRunner.cs
+++ src/Ainsley.Core/Tasks/PowershellRunner.cs
@@ -1,10 +1,34 @@
-﻿namespace Ainsley.Core.Tasks
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Ainsley.Core.Tasks
 {
     public class PowershellRunner
     {
-        public string RunCommand()
+        public string RunCommand(string command)
         {
-            return "";
+            var outputBuilder = new StringBuilder();
+
+            var startInfo = new ProcessStartInfo("powershell.exe");
+            startInfo.Arguments = command;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.CreateNoWindow = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.UseShellExecute = false;
+
+            var process = new Process();
+            process.ErrorDataReceived += (sender, args) => outputBuilder.AppendLine(args.Data);
+            process.OutputDataReceived += (sender, args) => outputBuilder.AppendLine(args.Data);
+            process.EnableRaisingEvents = true;
+            process.StartInfo = startInfo;
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit((int) TimeSpan.FromMinutes(1).TotalMilliseconds);
+            process.CancelOutputRead();
+
+            return outputBuilder.ToString();
         }
     }
 }
---
+++ src/Ainsley.Tests/Unit/Tasks/PowershellRunnerTests.cs
@@ -0,0 +1,22 @@
+﻿using Ainsley.Core.Tasks;
+using NUnit.Framework;
+
+namespace Ainsley.Tests.Unit.Tasks
+{
+    [TestFixture]
+	public class PowershellRunnerTests
+    {
+        [Test]
+        public void should_capture_stderr_and_stdout()
+        {
+            // given
+            var runner = new PowershellRunner();
+
+            // when
+            string output = runner.RunCommand("dir ~/");
+
+            // then
+            System.Console.WriteLine(output);
+        }
+    }
+}
