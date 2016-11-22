e84009f334d006e37cf325e9bad3f2ad64ce21b3
Chris Small <chris.small@totaljobsgroup.com>
2016-11-22 17:00:11 +0000

    console: test coverage

---
+++ src/Ainsley.Tests/Integration/CommandLineRunnerTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace Ainsley.Tests.Integration
+{
+    [TestFixture]
+    public class CommandLineRunnerTests
+	{
+        [Test]
+        public void should_use_ainsley_yaml_file_by_default()
+        {
+            // given
+
+            // when
+
+            // then
+        }
+
+		[Test]
+		public void should_check_local_yaml_file_exists()
+		{
+			// given
+
+			// when
+
+			// then
+		}
+
+		[Test]
+		public void should_log_bad_config_file_format()
+		{
+			// given
+
+			// when
+
+			// then
+		}
+
+		[Test]
+		public void should_run_tasks()
+		{
+			// given
+
+			// when
+
+			// then
+		}
+	}
+}
---
+++ src/Ainsley.Tests/Integration/ProgramTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Ainsley.Tests.Integration
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        [Test]
+        public void should()
+        {
+            // given
+
+            // when
+
+            // then
+        }
+    }
+}
