0cd8380ce15b19abf8332541b42995b307dd3e83
Chris Small <chris.small@totaljobsgroup.com>
2016-11-25 13:14:15 +0000

    plugins: nuget installer prototype

---
+++ src/Remy.Tests/Integration/PluginInstallerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Remy.Core.Tasks;
+
+namespace Remy.Tests.Integration
+{
+	[TestFixture]
+	public class PluginInstallerTests
+	{
+		[Test]
+		public void should()
+		{
+			// given
+			var pluginInstaller = new PluginInstaller();
+
+			// when
+			pluginInstaller.Run();
+
+			// then
+		}
+	}
+}
