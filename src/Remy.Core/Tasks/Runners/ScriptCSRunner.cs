6a50d81ec501b1ff912b57bbe5f2a99eb1096858
Chris S <mrshrinkray@gmail.com>
2016-11-25 10:41:21 +0000

    nuget: added scriptcs and runner

---
+++ src/Remy.Core/Tasks/Runners/ScriptCSRunner.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using ScriptCs;
+using ScriptCs.Contracts;
+using ScriptCs.Hosting;
+using Serilog;
+using ScriptCs.Engine.Roslyn;
+
+namespace Remy.Core.Tasks.Runners
+{
+	public class LogP : ILogProvider
+	{
+		private readonly ILogger _logger;
+
+		public LogP(ILogger logger)
+		{
+			_logger = logger;
+		}
+
+		public Logger GetLogger(string name)
+		{
+			return (level, gettext, exception, args) =>
+			{
+				_logger.Information(gettext());
+				return true;
+			};
+			throw new NotImplementedException();
+		}
+
+		public IDisposable OpenNestedContext(string message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IDisposable OpenMappedContext(string key, string value)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class ScriptCsRunner
+	{
+        private readonly ILogger _logger;
+
+        public ScriptCsRunner(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+		public bool Run(string code)
+		{
+			var console = (IConsole)new ScriptConsole();
+			var logProvider = new ColoredConsoleLogProvider(LogLevel.Info, console);
+
+			var builder = new ScriptServicesBuilder(console, logProvider);
+			builder.ScriptEngine<CSharpScriptEngine>();
+
+			var services = builder.Build();
+
+			var executor = (ScriptExecutor)services.Executor;
+			executor.Initialize(Enumerable.Empty<string>(), Enumerable.Empty<IScriptPack>());
+			Execute(executor);
+
+			return true;
+		}
+
+		public void Execute(ScriptExecutor executor)
+		{
+			//executor.Execute("HelloWorld.csx");
+			var script = @"Console.WriteLine(""Hello from scriptcs"")";
+			var result = executor.ExecuteScript(script);
+		}
+	}
+}
---
+++ src/Remy.Tests/Integration/ScriptCsRunnerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Remy.Core.Tasks.Runners;
+
+namespace Remy.Tests.Integration
+{
+	public class ScriptCsRunnerTests
+	{
+		[Test]
+		public void should()
+		{
+			// Arrange
+			var runner = new ScriptCsRunner(null);
+
+			// Act
+			runner.Run("test);");
+
+			// Assert
+		}
+	}
+}
