e72872c721c96eb4bbdcc27194203f362c4dfd3f
Chris Small <chris.small@totaljobsgroup.com>
2016-12-05 16:47:08 +0000

    forgot to hit save on csproj...

---
+++ src/Remy.Console/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = "Stop";
+
+# http://onlinemd5.com/ to generate the SHA1
+# TODO: generate SHA1 automatically
+$packageName = "remy"
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$checkSum = "TODO"
+
+$url = "github release url here"
+
+Install-ChocolateyZipPackage "packageName" -url "$url" -unzipLocation "$toolsDir" -checksumType "sha1" -checksum "$checkSum"
