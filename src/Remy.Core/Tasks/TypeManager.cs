aee2e4a783d2bb75b2a2bdee665ac9c414b43edd
Chris Small <chris.small@totaljobsgroup.com>
2016-12-05 17:33:16 +0000

    plugins: cleanup of Typemanager

--- src/Remy.Core/Tasks/TypeManager.cs
+++ src/Remy.Core/Tasks/TypeManager.cs
@@ -48,30 +48,10 @@ namespace Remy.Core.Tasks
 			{
 				registeredTasks.Add(task.YamlName, task);
 
-				logger.Information($"Found '{task.GetType().Name}' for '{task.YamlName}'");
+				logger.Information($"Registered '{task.GetType().Name}' for '{task.YamlName}'");
 			}
 
 	        return registeredTasks;
         }
-
-		void old()
-	    {
-			//IEnumerable<Type> taskTypes = type.Assembly
-			//	.GetTypes()
-			//	.Where(x => type.IsAssignableFrom(x) && x.IsClass);
-
-			//var registeredTasks = new Dictionary<string, ITask>();
-			//foreach (Type taskType in taskTypes)
-			//{
-			//	var instance = Activator.CreateInstance(taskType);
-
-			//	ITask taskInstance = instance as ITask;
-			//	registeredTasks.Add(taskInstance.YamlName, taskInstance);
-
-			//	logger.Information($"Found '{taskType.Name}' for '{taskInstance.YamlName}'");
-			//}
-
-			//return registeredTasks;
-		}
     }
 }
