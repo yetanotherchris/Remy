26ae1f7ac1dbf09c7ad8590db563bb6d0ff4453c
Chris S <mrshrinkray@gmail.com>
2016-12-06 09:46:02 +0000

    plugins: simplified typemanager tests

--- src/Remy.Tests/Integration/TypeManagerTests.cs
+++ src/Remy.Tests/Integration/TypeManagerTests.cs
@@ -7,17 +7,18 @@ using Remy.Core.Tasks;
 using Remy.Tests.StubsAndMocks;
 using Serilog;
 
-namespace Remy.Tests.Unit.Core.Tasks
+namespace Remy.Tests.Integration
 {
 	[TestFixture]
+	[Category("Foo")]
 	public class TypeManagerTests
 	{
 		private ILogger _logger;
 		private string _currentDir;
 		private string _pluginsDirectory;
 
-		[SetUp]
-		public void Setup()
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
 		{
 			_logger = new LoggerConfiguration()
 				.WriteTo
@@ -27,53 +28,27 @@ namespace Remy.Tests.Unit.Core.Tasks
 			_currentDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
 			_pluginsDirectory = Path.Combine(_currentDir, "plugins");
 
-			try
-			{
-				if (Directory.Exists(_pluginsDirectory))
-					Directory.Delete(_pluginsDirectory, true);
-				
-				Directory.CreateDirectory(_pluginsDirectory);
-			}
-			catch
-			{
-			}
-		}
-
-		[TearDown]
-		public void TearDown()
-		{
-			try
-			{
+			if (Directory.Exists(_pluginsDirectory))
 				Directory.Delete(_pluginsDirectory, true);
-			}
-			catch
-			{
-			}
-		}
+				
+			Directory.CreateDirectory(_pluginsDirectory);
 
-		[Test]
-		public void should_return_all_itasks_and_register_yamlname_for_keys()
-		{
-			// given + when
-			Dictionary<string, ITask> tasks = TypeManager.GetRegisteredTaskInstances(_logger);
-
-			// then
-			Assert.That(tasks.Count, Is.EqualTo(4));
-
-			KeyValuePair<string, ITask> task = tasks.First();
-			Assert.That(task.Value, Is.Not.Null);
-			Assert.That(task.Key, Is.EqualTo(task.Value.YamlName));
-		}
-
-		[Test]
-		public void should_add_tasks_from_plugins_directory()
-		{
-			// given - copy MockTask (Remy.tests.dll) into plugins/
+			// copy MockTask (Remy.tests.dll)into plugins /
 			string nugetFolder = Path.Combine(_pluginsDirectory, "Remy.tests", "lib", "net461");
 			Directory.CreateDirectory(nugetFolder);
 			File.Copy(Path.Combine(_currentDir, "Remy.tests.dll"), Path.Combine(nugetFolder, "Remy.tests.dll"));
+		}
 
-			// when
+		[OneTimeTearDown]
+		public void OneTimeTearDown()
+		{
+			Directory.Delete(_pluginsDirectory, true);
+		}
+		
+		[Test]
+		public void should_add_tasks_from_plugins_directory()
+		{
+			// given + when
 			Dictionary<string, ITask> tasks = TypeManager.GetRegisteredTaskInstances(_logger);
 
 			// then
