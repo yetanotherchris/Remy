58c07187383097f1ea109a454cf57c1d4c359b3b
Chris Small <chris.small@totaljobsgroup.com>
2016-12-06 16:36:51 +0000

    appveyor: chocolatey push scripts and yml deploy step

---
+++ src/Remy.Console/chocolateyinstall.template.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = "Stop";
+
+$packageName = "remy"
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$checkSum = "{CHECKSUM}"
+
+$url = "https://github.com/yetanotherchris/Remy/releases/download/remy-v{VERSION}/Remy-{VERSION}.zip"
+
+Install-ChocolateyZipPackage "packageName" -url "$url" -unzipLocation "$toolsDir" -checksumType "sha256" -checksum "$checkSum"
