bfd5d0df7342b2b488c89b4dcdca0b22e8fbfe4a
Chris Small <chris.small@totaljobsgroup.com>
2016-12-07 10:53:29 +0000

    tests: better coverage for powershelltask, and DI

--- src/Remy.Tests/Unit/Core/Tasks/Plugins/PowershellTaskTests.cs
+++ src/Remy.Tests/Unit/Core/Tasks/Plugins/PowershellTaskTests.cs
@@ -1,42 +1,86 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using NUnit.Framework;
 using Remy.Core.Tasks.Plugins;
+using Remy.Tests.StubsAndMocks.Core.Tasks.Runners;
+using Serilog;
+using Serilog.Core;
 
 namespace Remy.Tests.Unit.Core.Tasks.Plugins
 {
     [TestFixture]
 	public class PowershellTaskTests
     {
-        [Test]
+		private PowershellRunnerMock _powershellMock;
+		private StringBuilder _logStringBuilder;
+		private Logger _logger;
+
+		[SetUp]
+		public void Setup()
+		{
+			_powershellMock = new PowershellRunnerMock();
+
+			_logStringBuilder = new StringBuilder();
+			var logMessages = new StringWriter(_logStringBuilder);
+
+			_logger = new LoggerConfiguration()
+				.WriteTo
+				.TextWriter(logMessages)
+				.WriteTo
+				.LiterateConsole()
+				.CreateLogger();
+		}
+
+		[Test]
         public void should_have_yaml_name()
         {
-            Assert.That(new PowershellTask().YamlName, Is.EqualTo("powershell"));
+            Assert.That(new PowershellTask(_powershellMock).YamlName, Is.EqualTo("powershell"));
         }
 
         [Test]
         public void SetConfiguration_should_set_config_from_properties()
         {
             // given
-            var task = new PowershellTask();
+            var task = new PowershellTask(_powershellMock);
             var config = new PowershellTaskConfig();
+			var commands = new List<object>(){ "ls", "echo hello" };
 
-            var properties = new Dictionary<object, object>();
-            properties["commands"] = new List<object>()
-            {
-                "ls",
-                "echo hello"
-            };
+			var properties = new Dictionary<object, object>();
+	        properties["commands"] = commands;
 
-            // when
-            task.SetConfiguration(config, properties);
+			// when
+			task.SetConfiguration(config, properties);
 
             // then
             var actualconfig = task.Config as PowershellTaskConfig;
             Assert.That(actualconfig, Is.Not.Null);
 
-            Assert.That(actualconfig.Commands.Count, Is.EqualTo(2));
-            Assert.That(actualconfig.Commands[0], Is.EqualTo("ls"));
-            Assert.That(actualconfig.Commands[1], Is.EqualTo("echo hello"));
-        }
-    }
+			Assert.That(actualconfig.Commands.Count, Is.EqualTo(2));
+			Assert.That(actualconfig.Commands[0], Is.EqualTo("ls"));
+			Assert.That(actualconfig.Commands[1], Is.EqualTo("echo hello"));
+		}
+
+		[Test]
+		public void Run_should_set_powershell_runner_commands()
+		{
+			// given
+			var task = new PowershellTask(_powershellMock);
+			var config = new PowershellTaskConfig();
+			var commands = new List<object>() { "ls", "echo hello" };
+
+			var properties = new Dictionary<object, object>();
+			properties["commands"] = commands;
+			task.SetConfiguration(config, properties);
+
+			// when
+			task.Run(_logger);
+
+			// then
+			Assert.That(_powershellMock.Commands.Length, Is.EqualTo(2));
+			Assert.That(_powershellMock.Commands[0], Is.EqualTo(commands[0]));
+			Assert.That(_powershellMock.Commands[1], Is.EqualTo(commands[1]));
+		}
+	}
 }
