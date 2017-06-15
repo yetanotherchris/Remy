dd53cf23d199179eaf2f6fefcfd07a72513480d6
Chris S <chrisasmall+github@gmail.com>
2017-06-15 15:25:02 +0100

    Added info to the logger about which vars are replaced (needs hiding of sensitive data in future)

--- src/Remy.Core/Config/ConfigFileReader.cs
+++ src/Remy.Core/Config/ConfigFileReader.cs
@@ -1,78 +1,101 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using Serilog;
 
 namespace Remy.Core.Config
 {
-	public class ConfigFileReader : IConfigFileReader
-	{
-		public string Read(Uri uri)
-		{
-			if (uri.IsFile)
-			{
-				string yaml = ReadLocalFile(uri);
-				yaml = ReplaceAllEnvironmentalVariables(yaml);
+    public class ConfigFileReader : IConfigFileReader
+    {
+        private readonly ILogger _logger;
 
-				return yaml;
-			}
-			else if (uri.Scheme.StartsWith("http"))
-			{
-				string yaml = ReadRemoteFile(uri);
-				yaml = ReplaceAllEnvironmentalVariables(yaml);
-
-				return yaml;
-			}
-
-			throw new RemyException($"The scheme {uri.Scheme} for {uri} is not supported");
-		}
-
-		private string ReadLocalFile(Uri uri)
-		{
-			try
-			{
-				return File.ReadAllText(uri.LocalPath);
-			}
-			catch (IOException e)
-			{
-				throw new RemyException($"Unable to download open file '{uri}' - {e.Message}", e);
-			}
-		}
-
-		private string ReadRemoteFile(Uri uri)
-		{
-			try
-			{
-				return new WebClient().DownloadString(uri);
-			}
-			catch (WebException e)
-			{
-				throw new RemyException($"Unable to download '{uri}' - {e.Message}", e);
-			}
-		}
-
-		private string ReplaceAllEnvironmentalVariables(string yaml)
-		{
-			yaml = ReplaceEnvironmentalVariables(yaml, EnvironmentVariableTarget.User);
-			yaml = ReplaceEnvironmentalVariables(yaml, EnvironmentVariableTarget.Process);
-			yaml = ReplaceEnvironmentalVariables(yaml, EnvironmentVariableTarget.Machine);
-
-			return yaml;
-		}
-
-		private string ReplaceEnvironmentalVariables(string yaml, EnvironmentVariableTarget target)
-		{
-			var envVars = Environment.GetEnvironmentVariables(target);
-			foreach (string key in envVars.Keys)
-			{
-				yaml = yaml.Replace("{{" + key + "}}", envVars[key].ToString());
-			}
-
-			return yaml;
-		}
-
-	    public static string GetFullPathForFile(string filename)
-	    {
-			return Path.Combine(Directory.GetCurrentDirectory(), filename);
+        public ConfigFileReader(ILogger logger)
+        {
+            _logger = logger;
+            _logger.Warning("test");
         }
-	}
+
+        public string Read(Uri uri)
+        {
+            if (uri.IsFile)
+            {
+                string yaml = ReadLocalFile(uri);
+                yaml = ReplaceAllEnvironmentalVariables(yaml);
+
+                return yaml;
+            }
+            else if (uri.Scheme.StartsWith("http"))
+            {
+                string yaml = ReadRemoteFile(uri);
+                yaml = ReplaceAllEnvironmentalVariables(yaml);
+
+                return yaml;
+            }
+
+            throw new RemyException($"The scheme {uri.Scheme} for {uri} is not supported");
+        }
+
+        private string ReadLocalFile(Uri uri)
+        {
+            try
+            {
+                return File.ReadAllText(uri.LocalPath);
+            }
+            catch (IOException e)
+            {
+                throw new RemyException($"Unable to download open file '{uri}' - {e.Message}", e);
+            }
+        }
+
+        private string ReadRemoteFile(Uri uri)
+        {
+            try
+            {
+                return new WebClient().DownloadString(uri);
+            }
+            catch (WebException e)
+            {
+                throw new RemyException($"Unable to download '{uri}' - {e.Message}", e);
+            }
+        }
+
+        private string ReplaceAllEnvironmentalVariables(string yaml)
+        {
+            yaml = ReplaceEnvironmentalVariables(yaml, EnvironmentVariableTarget.User);
+            yaml = ReplaceEnvironmentalVariables(yaml, EnvironmentVariableTarget.Process);
+            yaml = ReplaceEnvironmentalVariables(yaml, EnvironmentVariableTarget.Machine);
+
+            return yaml;
+        }
+
+        private string ReplaceEnvironmentalVariables(string yaml, EnvironmentVariableTarget target)
+        {
+            var envVars = Environment.GetEnvironmentVariables(target);
+            _logger.Information($"Found {envVars.Count} '{target}' environment variables");
+            foreach (string key in envVars.Keys)
+            {
+                string keyToken = "{{" + key + "}}";
+
+                if (yaml.Contains(keyToken))
+                {
+                    string value = envVars[key].ToString();
+                    string displayValue = value;
+                    if (displayValue.Length > 10)
+                    {
+                        displayValue = displayValue.Substring(0, 10) + "...";
+                    }
+
+                    _logger.Information($"Found env variable '{keyToken}' in YAML and replacing it with '{displayValue}'");
+                    yaml = yaml.Replace("{{" + key + "}}", envVars[key].ToString());
+                }
+            }
+
+            return yaml;
+        }
+
+        public static string GetFullPathForFile(string filename)
+        {
+            return Path.Combine(Directory.GetCurrentDirectory(), filename);
+        }
+    }
 }
--- src/Remy.Tests/Integration/Core/Config/ConfigFileReaderTests.cs
+++ src/Remy.Tests/Integration/Core/Config/ConfigFileReaderTests.cs
@@ -10,141 +10,150 @@ using Nancy;
 using NUnit.Framework;
 using Remy.Core;
 using Remy.Core.Config;
+using Serilog;
+using Serilog.Core;
 
 namespace Remy.Tests.Integration.Core.Config
 {
-	[TestFixture]
-	public class ConfigFileReaderTests
-	{
-		[Test]
-		public void should_read_file_from_disk()
-		{
-			// Arrange
-			string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test-config.yml");
-			var fileReader = new ConfigFileReader();
+    [TestFixture]
+    public class ConfigFileReaderTests
+    {
+        private Logger _logger;
+        private ConfigFileReader _configFileReader;
 
-			// Act
-			string actualContent = fileReader.Read(new Uri(configPath));
+        [SetUp]
+        public void Setup()
+        {
+            _logger = new LoggerConfiguration()
+                .WriteTo
+                .LiterateConsole()
+                .CreateLogger();
 
-			// Assert
-			Assert.That(actualContent, Does.Contain("name: \"Text example\""));
-		}
+            _configFileReader = new ConfigFileReader(_logger);
+        }
 
-		[Test]
-		public void should_read_file_from_url()
-		{
-			// Arrange
-			string url = GetLocalhostAddress();
+        [Test]
+        public void should_read_file_from_disk()
+        {
+            // Arrange
+            string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test-config.yml");
 
-			using (var task = RunBasicHttpServer(url, "- some yaml\n- more yaml"))
-			{
-				var fileReader = new ConfigFileReader();
+            // Act
+            string actualContent = _configFileReader.Read(new Uri(configPath));
 
-				// Act
-				string actualContent = fileReader.Read(new Uri(url));
+            // Assert
+            Assert.That(actualContent, Does.Contain("name: \"Text example\""));
+        }
 
-				// Assert
-				Assert.That(actualContent, Does.Contain("- some yaml\n- more yaml"));
-			}
-		}
+        [Test]
+        public void should_read_file_from_url()
+        {
+            // Arrange
+            string url = GetLocalhostAddress();
 
-		[Test]
-		public void should_read_file_from_url_and_replace_env_vars()
-		{
-			// Arrange
-			string url = GetLocalhostAddress();
+            using (var task = RunBasicHttpServer(url, "- some yaml\n- more yaml"))
+            {
+                // Act
+                string actualContent = _configFileReader.Read(new Uri(url));
 
-			Environment.SetEnvironmentVariable("key1", "myvalue1", EnvironmentVariableTarget.Machine);
-			Environment.SetEnvironmentVariable("key2", "myvalue2", EnvironmentVariableTarget.Process);
-			Environment.SetEnvironmentVariable("key3", "myvalue3", EnvironmentVariableTarget.User);
-			Environment.SetEnvironmentVariable("key1", "user values trump machine values", EnvironmentVariableTarget.User);
+                // Assert
+                Assert.That(actualContent, Does.Contain("- some yaml\n- more yaml"));
+            }
+        }
 
-			using (var task = RunBasicHttpServer(url, "- some yaml {{key1}} {{key2}} {{key3}}"))
-			{
-				// Server
-				var fileReader = new ConfigFileReader();
+        [Test]
+        public void should_read_file_from_url_and_replace_env_vars()
+        {
+            // Arrange
+            string url = GetLocalhostAddress();
 
-				// Act
-				string actualContent = fileReader.Read(new Uri(url));
+            Environment.SetEnvironmentVariable("key1", "myvalue1", EnvironmentVariableTarget.Machine);
+            Environment.SetEnvironmentVariable("key2", "myvalue2", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("key3", "myvalue3", EnvironmentVariableTarget.User);
+            Environment.SetEnvironmentVariable("key1", "user values trump machine values", EnvironmentVariableTarget.User);
 
-				// Assert
-				Assert.That(actualContent, Does.Contain("myvalue2"));
-				Assert.That(actualContent, Does.Contain("myvalue3"));
-				Assert.That(actualContent, Does.Contain("user values trump machine values"));
-			}
-		}
+            using (var task = RunBasicHttpServer(url, "- some yaml {{key1}} {{key2}} {{key3}}"))
+            {
+                // Arrange
 
-		private string GetLocalhostAddress()
-		{
-			var listener = new TcpListener(IPAddress.Loopback, 0);
-			listener.Start();
-			int port = ((IPEndPoint)listener.LocalEndpoint).Port;
-			listener.Stop();
+                // Act
+                string actualContent = _configFileReader.Read(new Uri(url));
 
-			return $"http://localhost:{port}/";
-		}
+                // Assert
+                Assert.That(actualContent, Does.Contain("myvalue2"));
+                Assert.That(actualContent, Does.Contain("myvalue3"));
+                Assert.That(actualContent, Does.Contain("user values trump machine values"));
+            }
+        }
 
-		public static Task RunBasicHttpServer(string url, string outputHtml)
-		{
-			return Task.Run(() =>
-			{
-				HttpListener listener = new HttpListener();
-				listener.Prefixes.Add(url);
-				listener.Start();
+        private string GetLocalhostAddress()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            int port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
 
-				// GetContext method blocks while waiting for a request. 
-				HttpListenerContext context = listener.GetContext();
-				HttpListenerResponse response = context.Response;
+            return $"http://localhost:{port}/";
+        }
 
-				Stream stream = response.OutputStream;
-				var writer = new StreamWriter(stream);
-				writer.Write(outputHtml);
-				writer.Close();
-			});
-		}
+        public static Task RunBasicHttpServer(string url, string outputHtml)
+        {
+            return Task.Run(() =>
+            {
+                HttpListener listener = new HttpListener();
+                listener.Prefixes.Add(url);
+                listener.Start();
 
-		[Test]
-		public void GetFullPathForFile_should_return_full_path_for_filename()
-		{
-			// Arrange
-			string expectedPath = Path.Combine(Directory.GetCurrentDirectory(), "myfile.txt");
+                // GetContext method blocks while waiting for a request.
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerResponse response = context.Response;
 
-			// Act
-			string actualPath = ConfigFileReader.GetFullPathForFile("myfile.txt");
+                Stream stream = response.OutputStream;
+                var writer = new StreamWriter(stream);
+                writer.Write(outputHtml);
+                writer.Close();
+            });
+        }
 
-			// Assert
-			Assert.That(actualPath, Is.EqualTo(expectedPath));
-		}
+        [Test]
+        public void GetFullPathForFile_should_return_full_path_for_filename()
+        {
+            // Arrange
+            string expectedPath = Path.Combine(Directory.GetCurrentDirectory(), "myfile.txt");
 
-		[Test]
-		public void should_throw_remyexception_for_bad_filepath()
-		{
-			// Arrange
-			string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "doesnexist");
-			var fileReader = new ConfigFileReader();
+            // Act
+            string actualPath = ConfigFileReader.GetFullPathForFile("myfile.txt");
 
-			// Act + Assert
-			Assert.Throws<RemyException>(() => fileReader.Read(new Uri(configPath)));
-		}
+            // Assert
+            Assert.That(actualPath, Is.EqualTo(expectedPath));
+        }
 
-		[Test]
-		public void should_throw_remyexception_for_invalid_uri()
-		{
-			// Arrange
-			var fileReader = new ConfigFileReader();
+        [Test]
+        public void should_throw_remyexception_for_bad_filepath()
+        {
+            // Arrange
+            string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "doesnexist");
 
-			// Act + Assert
-			Assert.Throws<RemyException>(() => fileReader.Read(new Uri("http://bleh")));
-		}
+            // Act + Assert
+            Assert.Throws<RemyException>(() => _configFileReader.Read(new Uri(configPath)));
+        }
 
-		[Test]
-		public void should_throw_remyexception_for_unsupported_scheme()
-		{
-			// Arrange
-			var fileReader = new ConfigFileReader();
+        [Test]
+        public void should_throw_remyexception_for_invalid_uri()
+        {
+            // Arrange
 
-			// Act + Assert
-			Assert.Throws<RemyException>(() => fileReader.Read(new Uri("ftp://warezplace")));
-		}
-	}
+            // Act + Assert
+            Assert.Throws<RemyException>(() => _configFileReader.Read(new Uri("http://bleh")));
+        }
+
+        [Test]
+        public void should_throw_remyexception_for_unsupported_scheme()
+        {
+            // Arrange
+
+            // Act + Assert
+            Assert.Throws<RemyException>(() => _configFileReader.Read(new Uri("ftp://warezplace")));
+        }
+    }
 }
